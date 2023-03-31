ccea4246a5dba482a092334a81dd7c350fe73b2e
Sechaba Mache <93662067+sechaba-mache@users.noreply.github.com>
2023-03-31 10:12:10 +0200

    fix: Removed unused code

--- angular-holiday-planner/src/app/components/register/register.component.ts
+++ angular-holiday-planner/src/app/components/register/register.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {IUser} from "../../models/user";
-import {catchError, first, Observable, switchMap} from "rxjs";
+import {catchError, first, switchMap} from "rxjs";
 import { UserCredential } from '@angular/fire/auth';
 import {fromPromise} from "rxjs/internal/observable/innerFrom";
 import {AuthService} from "../../services/auth.service";
@@ -16,19 +16,10 @@ export class RegisterComponent {
 
   constructor(private auth: AuthService, private router: Router) { }
 
-  register$: Observable<UserCredential> | null = null;
-
-  confirmPassword: string = '';
-
   registerForm = new FormGroup({
-    // username: new FormControl('', [Validators.required]),
     email: new FormControl('', [Validators.required, Validators.email]),
     password: new FormControl('', [Validators.required, Validators.pattern("^(?=.*[a-zA-Z])(?=.*[0-9])[A-Za-z0-9]+$"), Validators.minLength(8)])
   })
-
-  // get username() {
-  //   return this.registerForm.get("username")
-  // }
 
   get email() {
     return this.registerForm.get("email")
@@ -42,7 +33,6 @@ export class RegisterComponent {
     const user: IUser = {
       email: this.email?.value!,
       password: this.password?.value!,
-      // username: this.username?.value!
     }
 
     fromPromise(this.auth.registerUser(user)).pipe(
