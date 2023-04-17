2ba405988c9c0b51212988a4708734cf5f5b130b
Sechaba Mache <93662067+sechaba-mache@users.noreply.github.com>
2023-04-17 15:15:44 +0200

    fix: Small fix with auth service not finding the users credentials in session storage

--- angular-holiday-planner/src/app/services/auth/auth.service.ts
+++ angular-holiday-planner/src/app/services/auth/auth.service.ts
@@ -17,15 +17,26 @@ export class AuthService {
   user: User | null | undefined;
   loggedIn = false;
   constructor(private auth: Auth) {
-      this.user = JSON.parse(sessionStorage.getItem("firebase:authUser:AIzaSyDqnu4jzVtNQ1ywyoFsZCskGZd5jvXAGH4:[DEFAULT]") ?? "")
-  }
+    console.log(sessionStorage.key(0))
+    const storage = sessionStorage.key(0);
+
+    if(storage){
+      this.user = JSON.parse(sessionStorage.getItem(storage) ?? "")
+
+    }
+ }
 
   registerUser(user: IUser){
     return this.auth.setPersistence(browserSessionPersistence).then(() => createUserWithEmailAndPassword(this.auth, user.email, user.password))
   }
 
   login(user: IUser){
-    return this.auth.setPersistence(browserSessionPersistence).then(() => signInWithEmailAndPassword(this.auth, user.email, user.password).then(res => this.user = res.user));
+    return this.auth.setPersistence(browserSessionPersistence).then(() => signInWithEmailAndPassword(this.auth, user.email, user.password).then(res => {
+      // console.log(this.auth.currentUser)
+      // console.log(this.user)
+      // console.log(res.user)
+      return this.user = res.user
+    }));
   }
 
   logout() {
